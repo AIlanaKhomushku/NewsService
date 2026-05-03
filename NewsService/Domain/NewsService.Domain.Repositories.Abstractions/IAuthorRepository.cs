10a05b7993d211cdee1b057905cc2b5bce584c24
Ailana110a <ailankakhomushku@gmail.com>
2026-05-03 23:12:13 +0700

    added IRepository

---
+++ NewsService/Domain/NewsService.Domain.Repositories.Abstractions/IAuthorRepository.cs
@@ -0,0 +1,15 @@
+﻿using NewsService.Domain.NewsService.Domain;
+using NewsService.Domain.Repositories.Abstractions.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsService.Domain.Repositories.Abstractions;
+
+public interface IAuthorRepository : IRepository<User, Guid>
+{
+    // Так как имя пользователя уникальное
+    Task<Author?> GetUserByAuthornameAsync(string Authorname, CancellationToken cancellationToken);
+}
